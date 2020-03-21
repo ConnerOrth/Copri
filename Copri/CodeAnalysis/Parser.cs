4c36f5a9357a7abb4306ee1cac1a1cc3ee7aba5e
Conner Orth <10193903+ConnerOrth@users.noreply.github.com>
2020-03-21 18:53:14 +0100

    moved precedences to SyntaxFacts

--- Copri/CodeAnalysis/Parser.cs
+++ Copri/CodeAnalysis/Parser.cs
@@ -70,7 +70,7 @@ namespace Copri.CodeAnalysis
 
             while (true)
             {
-                int precedence = GetBinaryOperatorPrecedence(Current.Kind);
+                int precedence = Current.Kind.GetBinaryOperatorPrecedence();
                 if (precedence == 0 || precedence <= parentPrecedence) break;
 
                 SyntaxToken operatorToken = NextToken();
@@ -80,22 +80,7 @@ namespace Copri.CodeAnalysis
 
             return left;
         }
-
-        private static int GetBinaryOperatorPrecedence(SyntaxKind kind)
-        {
-            switch (kind)
-            {
-                case SyntaxKind.StarToken:
-                case SyntaxKind.SlashToken:
-                    return 2;
-                case SyntaxKind.PlusToken:
-                case SyntaxKind.MinusToken:
-                    return 1;
-                default:
-                    return 0;
-            }
-        }
-
+        
         private ExpressionSyntax ParsePrimaryExpression()
         {
             if (Current.Kind == SyntaxKind.OpenParenthesisToken)
